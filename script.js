c9349e7e4846b207adb8f9a4bf9bf7b41511117b
Nivando <sidvandoni@gmail.com>
2022-10-04 22:07:07 -0300

    added TSE url API

---
+++ script.js
@@ -0,0 +1,33 @@
+fetch(
+  "https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json"
+)
+  .then((response) => response.json())
+
+  .then((data) => {
+    nomes = data.cand.map(function (f) {
+      return f.nm;
+    });
+    totais = data.cand.map(function (f) {
+      return f.pvap;
+    });
+    console.log(nomes);
+
+    var options = {
+      chart: {
+        type: "bar",
+      },
+      series: [
+        {
+          name: "%",
+          data: totais,
+        },
+      ],
+      xaxis: {
+        categories: nomes,
+      },
+    };
+
+    var chart = new ApexCharts(document.querySelector("#chart"), options);
+
+    chart.render();
+  });
